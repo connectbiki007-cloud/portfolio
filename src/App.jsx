8b2684a6956d8f6554c477e99f145989058adf8e
Biki079 <bikashkc2057@gmail.com>
2026-02-23 07:04:57 +0545

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 
 const App = () => {
